ff2255ed261478631fe466cb522fe2baf9982a52
sergio.zorrilla <sergio.zorrilla@rappi.com>
2023-12-16 21:52:46 -0600

    Created project

---
+++ food-assessment-front-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food-assessment-front-iOS
+//
+//  Created by Sergio Eduardo Zorilla Arellano on 16/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
